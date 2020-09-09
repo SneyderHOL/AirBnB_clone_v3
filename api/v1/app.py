7c53b43fd485fa0523daa04e29e146cba91ccc5d
SneyderHOL <1507@holbertonschool.com>
2020-09-09 03:34:06 +0000

    [UPDATE]: add error 404 method to app

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 '''
 Starts the Flask web application
 '''
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import environ
@@ -14,6 +14,11 @@ app.register_blueprint(app_views)
 def teardown_storage(exception):
     """Closes storage session"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == '__main__':
