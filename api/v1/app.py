ad657877286a55828b9480fc61eb24e51323d522
SneyderHOL <1507@holbertonschool.com>
2020-09-09 23:39:55 +0000

    [UPDATE]: using CORS instance in api

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,11 +3,13 @@
 Starts the Flask web application
 '''
 from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import environ
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, origins="0.0.0.0")
 
 
 @app.teardown_appcontext
