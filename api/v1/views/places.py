1efa7ea15eddbcfd34569368bf28c7ddc1c281bd
andergcp <andergcp@hotmail.com>
2020-09-11 02:01:19 +0000

    [FIX] Importing State

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -9,6 +9,7 @@ from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.user import User
+from models.state import State
 from os import environ
 
 
